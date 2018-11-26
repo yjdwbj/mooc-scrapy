b35f9efaa5e68a32dcfbaaaa4d25a0dea2f861e5
yjdwbj <yjdwbj@gmail.com>
2018-11-26 17:36:57 +0800

    curl 断点续传不用,登录页面改变,每次有绑定手机的对话框

--- mooc-scrapy.py
+++ mooc-scrapy.py
@@ -15,6 +15,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import subprocess  
 
+capabilities = {'chrome.binary': '/usr/bin/google-chrome-stable'}
+
 
 
   
@@ -56,7 +58,8 @@ outdir = "output"
 cookies_file="cookies.ini"
 curl_cookies='curl_cookies.txt'
 user_agent = '''User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'''
-
+down_records = 'download_list.txt'
+done_list = set()
 urls = [
         #'https://www.icourse163.org/learn/UESTC-234010?tid=274005#/learn/content?type=detail&id=780283&cid=1003948427',
         #'https://www.icourse163.org/learn/ZJU-21001?tid=1001774003#/learn/content?type=detail&id=1002295118&cid=1002435215',
@@ -86,9 +89,23 @@ urls = [
         #高等数学（三）
         #'https://www.icourse163.org/learn/TONGJI-284001?tid=331001',
         #中国近现代史纲要
-        'https://www.icourse163.org/learn/ZJU-21001?tid=1001774003',
+        # 'https://www.icourse163.org/learn/ZJU-21001?tid=1001774003',
         #图片机器人操作系统入门
-        'https://www.icourse163.org/course/ISCAS-1002580008'
+        # 'https://www.icourse163.org/course/ISCAS-1002580008'
+        # 神经网络和深度学习
+        # 'https://www.icourse163.org/learn/DA-1002183004?tid=1002301011#/learn/announce',
+        # Python机器学习应用
+        # 'https://www.icourse163.org/learn/BIT-1001872001?tid=1001965001#/learn/announce',
+
+        #嵌入式系统与实验
+        # 'https://www.icourse163.org/learn/XMU-1001766012?tid=1003089002#/learn/announce',
+        #用Python玩转数据
+        'https://www.icourse163.org/learn/NJU-1001571005?tid=1003100001#/learn/announce',
+        #　改善深层神经网络：超参数调试、正则化以及优化
+        'https://www.icourse163.org/learn/DA-1002225001?tid=1002343001#/learn/announce',
+        #结构化机器学习项目
+        'https://www.icourse163.org/learn/DA-1002226001?tid=1002339003#/learn/announce'
+
         ]
 
 # 大学英语（口语）CAP_中国大学MOOC(慕课)
@@ -131,7 +148,9 @@ def lesson_list(chapterdir):
             continue
         
         print(lname)
+        lname = lname.replace('/', '&')
         lessondir = chapterdir + "/" + lname
+       
         if not os.path.exists(lessondir):
             os.mkdir(lessondir)
         section_list(lessondir)
@@ -150,7 +169,9 @@ def section_list(chapterdir):
         title = browser.find_elements_by_xpath('//*[@id="courseLearn-inner-box"]/div/div/div[3]/ul/li')[i].get_attribute('title')
         if '视频' not in title:
             continue
-        print('section title ',title)
+        print('section title ', title)
+        title = title.replace('/', '&')
+        time.sleep(3)
         #browser.get("https://www.icourse163.org/learn/NUDT-438002?tid=1002283003#/learn/content?type=detail&id=1003103232")
         #videosrc = browser.find_element_by_tag_name('source')
         #src = videosrc.get_attribute('src')
@@ -180,11 +201,20 @@ def section_list(chapterdir):
 #        browser.find_element_by_class_name('u-edu-h5player').send_keys(Keys.SPACE) # stop player 
 
         
-        foutput = chapterdir +"/"+title+".mp4"
+        foutput = chapterdir + "/" + title + ".mp4"
+        print("save to directiry ", foutput)
+        print('foutput in set: ', foutput in done_list)
+        if foutput in done_list:
+            print('download yet!!!!!!!!!!!!!!!!')
+            return
         print("save to directiry ",foutput)
-        print("download video ",src)
+        print("download video ", src)
+        
+        done_list.add(foutput)
+        if os.path.exists(foutput):
+            os.remove(foutput)
         cmds = ['wget','--load-cookies=%s' % cookies_file,'--wait=3','--read-timeout=10','-t','5','--user-agent="%s"' % user_agent,
-                '-c','%s' % src, '-O','%s' % foutput]
+                '%s' % src, '-O','%s' % foutput]
         curls = ['curl','-L','-b','%s' % curl_cookies,'-o','%s' % foutput,'-C','-','%s' %src]
         
         #child = subprocess.Popen(cmds,stdout=subprocess.PIPE)
@@ -194,12 +224,9 @@ def section_list(chapterdir):
             child = subprocess.Popen(cmds,stdout=subprocess.PIPE)
             print("download Giving up. retry......")
         print("download ok!!!!!!!!!!!")
+        with open(down_records, 'a+') as fd:
+            fd.write(foutput+'\n')
         
-        
-    
-
-
-    
 
 def email_login():
     user = input("126 email:")
@@ -207,30 +234,77 @@ def email_login():
     pwd = getpass()
     print("pwd","*****")
     browser.get('https://www.icourse163.org/')
-    time.sleep(5)
+    time.sleep(3)
     try:
         relogin = browser.find_element_by_id('j-reLogin')
         relogin.click()
     except NoSuchElementException:
         loginbtn = browser.find_element_by_class_name('m-index-person-loginBtn')
+        print('login button click!!!!')
         loginbtn.click()
     time.sleep(5)
-    urs = browser.find_element_by_id('j-ursContainer')
-    iframe1 = urs.find_element_by_xpath('.//iframe')
+
+    # urs = browser.find_element_by_class_name('ux-tabs-underline_hd')
+    urs = browser.find_elements_by_class_name('last-login-holder')
+    # print(urs.get_attribute('innerHTML'))
+    # print('list type:',type(urs))
+    urs[1].click()   #　点击邮箱登录.
+ 
+    time.sleep(2)
+
+    logincontainer = browser.find_element_by_id('j-ursContainer-0')
+    # 转入内置 iframe 示例　https://www.techbeamers.com/switch-between-iframes-selenium-python/
+    iframe1 = logincontainer.find_element_by_tag_name('iframe')
+    # iframe1 = browser.find_element_by_xpath('.//iframe')
     browser.switch_to_frame(iframe1)
     email = browser.find_element_by_name('email')
+    # email = browser.find_element_by_class_name('dlemail')
+    # print(email.get_attribute('innerHTML'))
     email.send_keys(user)
+    # //*[@id="auto-id-1542963569163"]/div[2]
+
     password = browser.find_element_by_name('password')
+    
+    # print(password.get_attribute('innerHTML'))
+
+
     password.send_keys(pwd)
-    dologin = browser.find_element_by_id('dologin')
+    print('get password!!!!')
+    # dologin = browser.find_element_by_id('dologin')
+    loginform = browser.find_element_by_id('login-form')
+    # print('form: ', loginform.get_attribute('innerHTML'))
+    # print('html: ',loginform.find_element_by_class_name('loginbox').get_attribute('innerHTML'))
+    # dologin = loginform.find_element_by_link_text('登录')
+    dologin = loginform.find_element_by_css_selector('#dologin')
+    # print(dologin.get_attribute('innerHTML'))
     dologin.click()
     time.sleep(3)
     browser.switch_to_default_content()
+    #  登录成功之后
+    time.sleep(4)
+    check_phone_bind()
     cookies = browser.get_cookies()
-    print("will write cookies type ",type(cookies))
-    print("cookies",cookies)
+    # print("will write cookies type ",type(cookies))
+    # print("cookies",cookies)
     json.dump(cookies,open(cookies_file,"w"))
     #pickle.dump( browser.get_cookies() , open("cookies.ini","wb"))
+
+def check_phone_bind():  # 绑定手机吗?
+    pbc = None
+    try:
+        pbc = browser.find_element_by_class_name('ux-modal_bd')
+    except NoSuchElementException:
+        pass
+
+    if not pbc:
+        print('not found ux-modal_bd')
+        return
+    print('get bind request!!!!!!')
+    # iframe2 = pbc.find_element_by_tag_name('iframe')
+    # browser.switch_to_frame(iframe2)
+    pbc.find_element_by_class_name("ignore-bind").click()
+    # browser.switch_to_default_content()
+   
 
 def check_login():
     browser.find_element_by_class_name('j-passportforplug')
@@ -275,32 +349,36 @@ def get_course(url):
     if chapterbox is None:
         return
         
-    print(chapterbox.get_attribute('innerHTML'))
+    # print(chapterbox.get_attribute('innerHTML'))
     chapters = browser.find_elements_by_xpath('//*[@id="courseLearn-inner-box"]/div/div/div[1]/div[1]/div/div[1]/div/div[2]/div ') 
     print("chapers size " , len(chapters))                       
-    for i in range(0,len(chapters)):
-        browser.find_element_by_xpath('//*[@id="courseLearn-inner-box"]/div/div/div[1]/div[1]/div/div[1]').click()
+    for i in range(0, len(chapters)):
+        try:
+            browser.find_element_by_xpath('//*[@id="courseLearn-inner-box"]/div/div/div[1]/div[1]/div/div[1]').click()
+        except NoSuchElementException:
+            pass
         # open list box
         chapters = browser.find_elements_by_xpath('//*[@id="courseLearn-inner-box"]/div/div/div[1]/div[1]/div/div[1]/div/div[2]/div') 
         chapters[i].click()
         time.sleep(2)
         chapters = browser.find_elements_by_xpath('//*[@id="courseLearn-inner-box"]/div/div/div[1]/div[1]/div/div[1]/div/div[2]/div') 
         cname = chapters[i].get_attribute('title')
-        print("open chapter ",cname)
+        print("open chapter ", cname) # 大章节
+        cname = cname.replace('/','&')
         chapterdir = lessondir + "/" + cname
         if not os.path.exists(chapterdir):
             os.mkdir(chapterdir)
         lesson_list(chapterdir)
 
 def dumpdict(x):
-    l = [];
+    l = []
     for key,value in x.items():
         print("key: ",key)
         print("val: ",value)
         temp="%s=%s;" % (key,value)
         l.append(temp)
     print("cookies list:",l)
-    print("-------------------------------------------------------")
+   
 
 if __name__ == '__main__':
     if not os.path.exists(outdir):
@@ -327,6 +405,8 @@ if __name__ == '__main__':
 
     browser.get('https://www.icourse163.org/')
     time.sleep(5)
+
+    # ul class ux-tabs-underline_hd
     try:
         browser.find_element_by_class_name('j-passportforplug')
     except NoSuchElementException:
@@ -335,6 +415,14 @@ if __name__ == '__main__':
             browser.add_cookie(cookie)
         email_login()
 
+    check_phone_bind()
+    try:
+        with open(down_records, 'r') as fd:
+            for line in fd:
+                done_list.add(line.strip('\n'))
+            print('donwload done list: ', len(done_list))
+    except:
+        pass
         
     for url in urls:
         print("get from",url)
